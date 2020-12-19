9a0c416707839ee4ec9d8f92a49599e547cde04d
DESKTOP-DMCKS0A\cc <nataila@163.com>
2020-12-19 16:23:51 +0800

    home qa

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -36,6 +36,15 @@ export default function Home () {
   ]
   const aData = [
     ' TurnitinUK是专为UK大学开发的，90%以上的UK大学都是使用TurnitinUK系统； Turnitin国际版与TurnitinUK的学生数据库有差别，因此检测的结果会不一样。 Turnitin国际版：使用范围最大，中国、美国、加拿大、澳洲、美洲、亚洲、欧洲（除英国外）、非洲、香港、澳门、台湾等世界上126个国家的学校都是使用这个系统来检测，Turnitin-UK版只有英国（UK）的大学使用。目前中国也有部分大学引进了Turnitin国际版系统来检测外语系的论文； 如果是在英国（UK）留学的请选择TurnitinUK来检测，在其他国家留学的请选择Turnitin国际版检测。也就是在英国留学的同学一定要选择TurnitinUK系统，不是在英国留学的都是选择Turnitin国际版； 如果需要到国际期刊（如EI、SCI）上发表论文，需要提前检测，请选择Turnitin国际版检测，Turnitin国际版还支持多语种检测，如中文、日语、西班牙语、德语、俄语、法语等小语种； 国内大学的中文学位论文检测，请使用国际版检测。',
+    'TurnitinUK是专为UK大学开发的，90%以上的UK大学都是使用TurnitinUK系统； Turnitin国际版与TurnitinUK的学生数据库有差别，因此检测的结果会不一样。 Turnitin国际版：使用范围最大，中国、美国、加拿大、澳洲、美洲、亚洲、欧洲（除英国外）、非洲、香港、澳门、台湾等世界上126个国家的学校都是使用这个系统来检测，Turnitin-UK版只有英国（UK）的大学使用。目前中国也有部分大学引进了Turnitin国际版系统来检测外语系的论文； 如果是在英国（UK）留学的请选择TurnitinUK来检测，在其他国家留学的请选择Turnitin国际版检测。也就是在英国留学的同学一定要选择TurnitinUK系统，不是在英国留学的都是选择Turnitin国际版； 如果需要到国际期刊（如EI、SCI）上发表论文，需要提前检测，请选择Turnitin国际版检测，Turnitin国际版还支持多语种检测，如中文、日语、西班牙语、德语、俄语、法语等小语种； 国内大学的中文学位论文检测，请使用国际版检测。',
+    '对的，我们为留学生提供全免费使用的查重与修改语法服务',
+    'OCcheck提供检测服务的检测接口为iParadigms官方特别定制接口，所检测论文不会被Turnitin数据库收录，请放心使用。 无论检测一次还是检测多次都不会被收录入库。',
+    '一般检测为15-30分钟即可出结果。如果字数多的文章会稍微延长一些。',
+    '系统提供HTML格式的检测结果报告，报告将标记抄袭的文字、抄袭来源、抄袭百分比（如下方图示显示），方便后期修改，通过学校检测。 带颜色的单词或者阴影的部分都是存在相似重复的，降低重复率必须修改这一部分； html文件中，同一颜色且标号相同的来源于“sources”中同一出处； "paper text"中，序号1、2、3...与“sources”部分序号1、2、3...一一对应的； Similarity Index指的是论文最终的重复率，其中包含互联网來源、出版物、学生文稿； Internet Sources指的是billions of active and archived web pages from the internet； Publications指的是a repository of frequently updated content from professional journals, periodicals, and publications；Student papers指的是a repository of papers previously submitted by Turnitin users；因为同一处文字以上三处都能检测到，所以三者相加的结果要超过相似度指标，三者可能存在交集，不是简单的叠加关系。',
+    'Turnitin系统检测对比数据库： 1.5亿的海量论文数据库；90000多种世界知名期刊杂志数据库；200多亿的网页数据库； 目前全世界超过10000所世界知名教育机构、超过2千万学生，1百万导师在使用Turnitin检测系统进行论文检测。',
+    '这一般由学校或者期刊编辑部来定； 低一些自然比较好，低于10%比较稳妥，但是有的学校对重复率有要求，过低也不符合要求。不同学校、不同专业学科之间，重复率也不能一概而论，请咨询学校、老师或者上届学长。',
+    '尽管Reference是一些固定的论文题目等，基本上检测都是重复且无法修改。不过，最好检测提交到学校的最终版本，能整体把握论文的重复率，避免含reference的版本提交到学校重复率过高；只检测正文可能出现某些文字无法查到。为了保持跟学校的检测一致，最好提交学校正式版本。',
+    '一般手法为替换句子中的关键字、关键字用同义替换。但是对专业性的论文有许多专业术语，无法替换，效果不明显；最好的方式是于OCcheck客服进行沟通，他们将为您匹配最合适的老师进行论文修改与润色，一般会使最终得分提高30%-50%。'
   ]
 
   return (
@@ -146,11 +155,11 @@ export default function Home () {
           </Col>
           <Col span={ 12 }>
             <div className="oc-shadow a-content">
+              { aData[qaIndex] }
             </div>
           </Col>
         </Row>
       </div>
-      <UseOccheck />
     </div>
   )
 }
