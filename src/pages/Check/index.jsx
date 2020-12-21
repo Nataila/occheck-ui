a0e9cea860ecd2967ce8b2e70971e4cbcf773d26
Nataila <ccfive814@gmail.com>
2020-12-21 17:45:21 +0800

    查重页面

--- src/pages/Check/index.jsx
+++ src/pages/Check/index.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import './index.sass';
+import UploadFile from '../../components/UploadFile';
+import BannerImg from '../../assets/imgs/banner.png';
 
 export default function Check () {
   return (
-    <div>check</div>
+    <div class="check-root top-banner">
+      <div className="banner">
+        <div className="banner-wrapper">
+          <div className="banner-doc">
+            <h1>立即查重</h1>
+            <h3>上传您的论文，获取一站式免费服务</h3>
+          </div>
+          <img src={BannerImg} alt="" />
+        </div>
+      </div>
+      <div style={{ padding: '40px 0'}}>
+        <UploadFile />
+      </div>
+    </div>
   )
 }
