e605ea4ea670e70c9cbbf5fe1dc53df730d4dd90
Nataila <ccfive814@gmail.com>
2020-12-30 18:39:55 +0800

    评论审核

--- src/pages/Admin/CommentList/index.jsx
+++ src/pages/Admin/CommentList/index.jsx
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Table } from 'antd';
+
+import api from '../../../api';
 import './index.sass';
 
 export default function CommentList() {
+  const [commentList, setComment] = useState([]);
+
+  async function getCommentList() {
+    const list = await api.commentList({limit: 18, status: 0});
+    setComment(list);
+  }
+
+  useEffect(() => {
+    getCommentList()
+  }, [])
+
+  const columns = [
+    {
+      title: '姓名',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: '内容',
+      dataIndex: 'content',
+      key: 'content',
+    },
+    {
+      title: '位置',
+      dataIndex: 'location',
+      key: 'location',
+    },
+  ]
+
   return (
-    <div>评论列表</div>
+    <Table dataSource={commentList} columns={columns} />
   )
 }
